501b25d5f0a12bd7a2a619b3aa29e1a4a521ed9b
ANUSUYAPERIYASWAMY <43032409+ANUSUYAPERIYASWAMY@users.noreply.github.com>
2018-09-07 12:35:01 +0530

    Add files via upload

---
+++ float.java
@@ -0,0 +1,13 @@
+class Float {
+  public static void main(String[] args) {
+    float a;
+    int b;
+    double A,S,M;
+    a=3.450f;
+    b=1;
+    A=a+b;
+    S=a-b;
+    M=a*b;
+    System.out.println(A+" "+S+" "+M);
+  }
+}
