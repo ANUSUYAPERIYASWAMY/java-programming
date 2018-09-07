1f91295dfc99bae4568755b1b570e95437000976
ANUSUYAPERIYASWAMY <43032409+ANUSUYAPERIYASWAMY@users.noreply.github.com>
2018-09-07 12:17:47 +0530

    Add files via upload

---
+++ doubleASM.java
@@ -0,0 +1,13 @@
+class Float {
+  public static void main(String[] args) {
+    double a;
+    int b;
+    double A,S,M;
+    a=3.450;
+    b=1;
+    A=a+b;
+    S=a-b;
+    M=a*b;
+    System.out.println(A+" "+S+" "+M);
+  }
+}
