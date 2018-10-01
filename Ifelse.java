c395dfc93ce8ace92f038f92844a0ac87aab579a
ANUSUYAPERIYASWAMY <43032409+ANUSUYAPERIYASWAMY@users.noreply.github.com>
2018-10-01 15:46:25 +0530

    Create Ifelse.java

---
+++ Ifelse.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ifelse
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=3000;
+    if(a>=20000)
+    System.out.println("Free trolley suitcase");
+    else
+     if(a>=10000)
+    System.out.println("Free travel bag");
+    else
+     if(a>=5000)
+    System.out.println("Free back bck");
+    else
+    System.out.println("collect money even for cover we give");
+
+	}
+}
