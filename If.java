a88df78ce5ec5efd96aecfbc0db3c730b8e164f9
ANUSUYAPERIYASWAMY <43032409+ANUSUYAPERIYASWAMY@users.noreply.github.com>
2018-10-01 15:47:59 +0530

    Create If.java

---
+++ If.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class If
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int DaysInTheartre=60;
+if(DaysInTheartre>=50)
+System.out.println("Box office Hit");
+if(DaysInTheartre<50)
+System.out.println("Flop");
+	}
+}
