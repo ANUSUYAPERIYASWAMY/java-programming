33876bebeea94558c5737105422247549c9e8ef5
ANUSUYAPERIYASWAMY <43032409+ANUSUYAPERIYASWAMY@users.noreply.github.com>
2018-10-03 12:09:06 +0530

    Create Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Leapyear
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int year;
+
+        for ( year = 1900; year <= 2020; year = year +4) {
+            System.out.println(year);}
+
+	}
+}
