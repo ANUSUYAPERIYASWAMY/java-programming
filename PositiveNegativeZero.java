2c64521e495b4bfdcc42e96f0fdfba6a4a42f7a7
ANUSUYAPERIYASWAMY <43032409+ANUSUYAPERIYASWAMY@users.noreply.github.com>
2018-10-04 11:39:25 +0530

    Create PositiveNegativeZero.java

---
+++ PositiveNegativeZero.java
@@ -0,0 +1,15 @@
+class PositiveNegativeZero {
+  public static void main(String[] args) {
+   int a=-2;
+   if(a>0)
+   {
+     System.out.println("Positve");
+   }
+   else if(a<0)
+   {
+     System.out.println("Negative");
+   }
+else
+System.out.println("Zero");
+  }
+}
