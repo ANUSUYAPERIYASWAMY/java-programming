426b3ae105d2361ad8feaeb331c7f98e18856b8e
ANUSUYAPERIYASWAMY <43032409+ANUSUYAPERIYASWAMY@users.noreply.github.com>
2018-10-04 12:40:13 +0530

    Create Vowel-consonant.java

---
+++ Vowel-consonant.java
@@ -0,0 +1,19 @@
+class Vowel-consonant {
+  public static void main(String[] args) {
+   char anu='a';
+   switch(anu)
+   {
+     case'a':
+     case'e':
+     case'i':
+     case'o':
+     case'u':
+    System.out.println("vowel");
+    break;
+   default:
+    
+   
+     System.out.println("consonant");
+   }
+  }
+  }
