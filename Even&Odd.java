73c9f5f7875d984804b8685ae6de7b469d961ebb
ANUSUYAPERIYASWAMY <43032409+ANUSUYAPERIYASWAMY@users.noreply.github.com>
2018-10-04 12:18:44 +0530

    Create Even&Odd.java

---
+++ Even&Odd.java
@@ -0,0 +1,13 @@
+class Even&Odd {
+  public static void main(String[] args) {
+   int a =0;
+   if(a%2==0)
+   {
+    System.out.println("Even");
+   }
+   else 
+   
+     System.out.println("Odd");
+   
+  }
+  }
