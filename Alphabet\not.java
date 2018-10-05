d5cd43f23a30aa14563b746da20c374b74d1f760
ANUSUYAPERIYASWAMY <43032409+ANUSUYAPERIYASWAMY@users.noreply.github.com>
2018-10-05 11:50:17 +0530

    Create Alphabet\not.java

---
+++ Alphabet\not.java
@@ -0,0 +1,11 @@
+class Alphabet\not{
+  public static void main(String[] args) {
+  char CH='T';
+  if((CH >='a'&&CH<='z')||(CH >='A'&&CH<='Z'))
+  {
+  System.out.println("IS AN ALPHABET");
+  }
+  else
+ System.out.println("NOT AN ALPHABET");
+  }
+}
