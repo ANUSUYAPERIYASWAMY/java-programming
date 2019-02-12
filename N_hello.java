f9b8b06dbe57276b448c6bc37f44439f948796a1
ANUSUYAPERIYASWAMY <43032409+ANUSUYAPERIYASWAMY@users.noreply.github.com>
2019-02-12 12:23:56 +0530

    Create N_hello.java

---
+++ N_hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class N_hello {
+  public static void main(String[] args)
+ {
+    int a,i;
+    System.out.println("enter a"); 
+    Scanner sc = new Scanner(System.in);
+ a = sc.nextInt();
+ 
+   for(i=0;i<a;i++)
+ {
+    System.out.println("Hello world!");
+  }
+}}
